b1d7fa2974fdf24a5c5c0d39523a3f81f926d98b
kboul <kos.voulgaridis@gmail.com>
2023-03-13 10:51:39 +0200

    Configure minimum code so redux-toolkit store is functional

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,10 @@
 // global state management code goes here
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
+import testReducer from './testSlice';
+
+const rootReducer = combineReducers({ test: testReducer });
+
+const store = configureStore({ reducer: rootReducer });
+
+export default store;
