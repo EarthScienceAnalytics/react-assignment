34273faded7b60e0515a71417e03f1cdff934c04
kboul <kos.voulgaridis@gmail.com>
2023-03-14 10:30:35 +0200

    Remove EsaButton styles

--- src/layouts/EsaButton/EsaButton.js
+++ src/layouts/EsaButton/EsaButton.js
@@ -1,30 +1,6 @@
 import PropTypes from "prop-types";
 import { Button, CircularProgress } from "@mui/material";
 
-// const AppButton = styled(Button)(({ theme }) => {
-//   return {
-//     color: theme.palette.default.contrastText,
-//     backgroundColor: styledBy("color", {
-//       default: theme.palette.default.dark,
-//       primary: theme.palette.primary.main,
-//       secondary: theme.palette.secondary.main,
-//       info: theme.palette.info.main,
-//       danger: theme.palette.danger.main,
-//       dark: "rgb(55,55,55)"
-//     }),
-//     "&:hover": {
-//       backgroundColor: styledBy("color", {
-//         default: theme.palette.default.main,
-//         primary: theme.palette.primary.dark,
-//         secondary: theme.palette.secondary.dark,
-//         info: theme.palette.info.dark,
-//         danger: theme.palette.danger.dark,
-//         dark: "rgba(55,55,55, .9)"
-//       })
-//     }
-//   };
-// });
-
 export default function EsaButton({
   classes,
   className,
@@ -35,11 +11,7 @@ export default function EsaButton({
   ...rest
 }) {
   return (
-    <Button
-      {...rest}
-      variant={variant}
-      // className={`${className} ${variant === "contained" ? classes.root : ""}`}
-    >
+    <Button {...rest} variant={variant}>
       {loading ? <CircularProgress size={24} color="inherit" /> : children}
     </Button>
   );
